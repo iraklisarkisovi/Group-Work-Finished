4eb5f4777bba71e6205ffc525916b67be190e56b
iraklisarkisovi <sarqisovirakli@gmail.com>
2024-09-18 17:21:01 +0400

    Commit

--- slider/slider.js
+++ slider/slider.js
@@ -2,13 +2,19 @@ const slider = document.querySelector('.slider');
 const nextButton = document.querySelector('.next-btn');
 const prevButton = document.querySelector('.prev-btn');
 const teamMembers = document.querySelectorAll('.team-member');
+const transform = document.querySelector('#transform')
+const transformBtn = document.querySelector('.transform-btn')
+const shadow = document.querySelector('.modal-shadow')
+const html = document.querySelector('html, body')
+const closeBtn = document.querySelector('.transform-btn-close')
+const closeBtnsecond = document.querySelector('.close-modal')
 
 let index = 0;
 
 nextButton.addEventListener('click', () => {
     if (index < teamMembers.length - 1) {
         index++;
-        slider.style.transform = `translateX(-${index * 100}%)`;
+        slider.style.transform = `translateX(-${index * 22}%)`;
     } else {
         index = 0;
         slider.style.transform = `translateX(0)`;
@@ -24,3 +30,28 @@ prevButton.addEventListener('click', () => {
         slider.style.transform = `translateX(-${index * 100}%)`;
     }
 });
+
+transformBtn.addEventListener('click', () => {
+    transform.style.transform = 'translateX(0)';
+    transform.style.transform = 'translateY(100px)';
+    shadow.style.display = 'block';
+    html.style.overflowY ='hidden';
+    closeBtn.style.display = 'block';
+    transformBtn.style.display ='none';
+})
+
+closeBtn.addEventListener('click', () => {
+    transform.style.transform = 'translateX(800px)';
+    shadow.style.display = 'none';
+    html.style.overflowY = 'visible';
+    closeBtn.style.display = 'none';
+    transformBtn.style.display ='block';
+})
+
+closeBtnsecond.addEventListener('click', () => {
+    transform.style.transform = 'translateX(800px)';
+    shadow.style.display = 'none';
+    html.style.overflowY = 'visible';
+    closeBtn.style.display = 'none';
+    transformBtn.style.display ='block';
+})
